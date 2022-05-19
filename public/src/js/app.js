d87ac647dddb87b35fb99a5e2761c5b540d6733b
Sihun Kim <skim585@asu.edu>
2022-05-18 18:40:35 -0700

    update service worker to cache files

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,48 +1,21 @@
-let deferredPrompt;
+let deferredPrompt
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(() => {
-    console.log("Service worker registered!");
-  });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+      console.log('Service worker registered!')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+} else {
+  console.log('Service worker not supported')
 }
 
-window.addEventListener("beforeinstallprompt", (e) => {
-  console.log("beforeinstallprompt fired");
-  e.preventDefault();
-  deferredPrompt = e;
-  return false;
-});
-
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve("This is executed once time is done");
-    reject({ code: 500, message: "An error occurred" });
-    // console.log("This is executed once time is done");
-  }, 3000);
-});
-
-// promise
-//   .then(
-//     (text) => {
-//       return text;
-//     },
-//     (err) => {
-//       console.log(err.code, err.message);
-//     }
-//   )
-//   .then((newText) => {
-//     console.log(newText);
-//   });
-
-promise
-  .then((text) => {
-    return text;
-  })
-  .then((newText) => {
-    console.log(newText);
-  })
-  .catch((err) => {
-    console.log(err.code, err.message);
-  });
-
-console.log("this is executed right after setTimeout");
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('beforeinstallprompt fired')
+  e.preventDefault()
+  deferredPrompt = e
+  return false
+})
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,45 @@
-self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Installing service worker...", event);
-});
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Installing service worker...', event)
+  event.waitUntil(
+    caches.open('statis').then((cache) => {
+      console.log('[Service Worker] Precaching App shell')
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ])
+    })
+  )
+})
 
-self.addEventListener("activate", (event) => {
-  console.log("[Service Worker] Activating service worker...", event);
-  return self.clients.claim();
-});
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activating service worker...', event)
+  return self.clients.claim()
+})
 
-self.addEventListener("fetch", (event) => {
-  console.log("[Service worker] Fetching something...", event);
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      if (res) {
+        return res
+      } else {
+        return fetch(event.request)
+          .then((response) => {
+            return caches.open('dynamic').then((cache) => {
+              cache.put(event.request.url, response.clone())
+              return response
+            })
+          })
+          .catch((err) => {})
+      }
+    })
+  )
+})
